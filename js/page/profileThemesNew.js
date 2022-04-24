3392e316fd3ab710974b9a5b57ffc9442115d97d
Ziggy <96440226+NotZiggy@users.noreply.github.com>
2022-04-24 21:46:59 +0200

    Add files via upload

---
+++ js/page/profileThemesNew.js
@@ -0,0 +1,262 @@
+/**
+
+RoPro (https://ropro.io) v1.3
+
+RoPro was wholly designed and coded by:
+                               
+,------.  ,--. ,-----.,------. 
+|  .-.  \ |  |'  .--./|  .---' 
+|  |  \  :|  ||  |    |  `--,  
+|  '--'  /|  |'  '--'\|  `---. 
+`-------' `--' `-----'`------' 
+                            
+Contact me:
+
+Discord - Dice#1000
+Email - dice@ropro.io
+Phone - 650-318-1631
+
+Write RoPro:
+
+Dice Systems LLC
+16192 Coastal Hwy
+Lewes, Deleware 19958
+United States
+
+RoPro Terms of Service:
+https://ropro.io/terms
+
+RoPro Privacy Policy:
+https://ropro.io/privacy-policy
+
+© 2022 Dice Systems LLC
+**/
+
+
+
+profileThemeBoxHTML = `<div class="section-content"><button id="saveTheme" type="button" class="btn-fixed-width-lg btn-growth-lg" style="background-color:#0084dd;border:0px;font-size:18px;padding:5px;float:right;">${chrome.i18n.getMessage("RemoveTheme")}</button><p id="themeResponseText"></p><div style="width:100%;float:left;margin-top:10px;"><h3 style="margin-bottom:-10px;">${chrome.i18n.getMessage("RoProProfileThemes")}</h3><h7 style="font-size:14px;margin-top:-10px;">${chrome.i18n.getMessage("OtherRoproUsersWillSeeYourThemeWhenTheyVisitYourProfile")}</h7><br><br></div><div style="min-height:150px" id="profileThemeCardBox"></div><br><div style="width:100%;float:left;margin-top:10px;"><h3 style="margin-bottom:-10px;">${chrome.i18n.getMessage("AnimatedThemes")}</h3><h7 style="font-size:14px;margin-top:-10px;">${chrome.i18n.getMessage("AnimatedThemesAreAvailableForSubscribersOnly")}</h7><br><br><div style="min-height:110px;" id="animatedThemeCardBox"></div></div></div>`
+mainProfileAnimationHTML = `<iframe style="position:absolute;width:100%;height:300%;top:-100%;left:0px;border:none;pointer-events:none;" src="https://ropro.io/animatetest01/?user=${getIdFromURL(location.href)}&mainicon"></iframe>`
+circularProfileAnimationHTML = `<div style="position:absolute;top:-0px;left:0px;width:100%;height:100%;overflow:hidden!important;border-radius:100px;pointer-events:none;"><iframe style="width:100%;height:100%;border:none;" src="https://ropro.io/animatetest01/?user=${getIdFromURL(location.href)}&circleicon"></iframe></div>`
+
+var ropro_themes = null
+
+function fetchThemes() {
+	return new Promise(resolve => {
+		chrome.runtime.sendMessage({greeting: "GetURL", url:"https://api.ropro.io/themesJSON.php"}, 
+			function(data) {
+				resolve(data)
+			}
+		)
+	})
+}
+
+function fetchSetting(setting) {
+	return new Promise(resolve => {
+		chrome.runtime.sendMessage({greeting: "GetSetting", setting: setting}, 
+			function(data) {
+				resolve(data)
+			}
+		)
+	})
+}
+
+function saveTheme(userID, themeName, remove) {
+	return new Promise(resolve => {
+		chrome.runtime.sendMessage({greeting: "GetURL", url:"https://api.ropro.io/saveTheme.php?userid=" + userID + "&themename=" + themeName}, 
+			function(data) {
+				resolve(data)
+			}
+		)
+	})
+}
+
+function getUserId() {
+	return new Promise(resolve => {
+		chrome.runtime.sendMessage({greeting: "GetUserID"}, 
+			function(data) {
+				resolve(data)
+			}
+		)
+	})
+}
+
+function getStorage(key) {
+	return new Promise(resolve => {
+		chrome.storage.sync.get(key, function (obj) {
+			resolve(obj[key])
+		})
+	})
+}
+
+function stripTags(s) {
+	if (typeof s == "undefined") {
+		return s
+	}
+	return s.replace(/(<([^>]+)>)/gi, "").replace(/</g, "").replace(/>/g, "").replace(/'/g, "").replace(/"/g, "").replace(/`/g, "");
+ }
+
+function addCard(theme) {
+	profileThemeCardHTML = `<div themejson='${encodeURI(JSON.stringify(theme))}' style="display:inline-block;width:50px;height:50px;margin:2px;margin-left:5px;margin-right:5px;margin-bottom:5px;" class="thumbnail-2d-container background-selector"><a style="position:relative;">
+<img class="item-card-thumb-container" style="width:100%;height:100%;" src="https://ropro.io/themes/low_quality/${stripTags(theme['name'])}">
+<img src="https://ropro.io/images/empty.png" class="background-checkmark-image active" style="pointer-events:none;transform:scale(0.4);overflow:visible;width:164px;height:126px;position:absolute;right:-57px;top:-52px;">
+</a></div>`
+	div = document.createElement('div')
+	div.innerHTML += profileThemeCardHTML
+	if ('animated' in theme) {
+		profileThemeCardBox = document.getElementById('animatedThemeCardBox')
+	} else {
+		profileThemeCardBox = document.getElementById('profileThemeCardBox')
+	}
+	profileThemeCardBox.appendChild(div)
+	$(div).click(function(){
+		theme = JSON.parse(decodeURI(this.getElementsByClassName('thumbnail-2d-container')[0].getAttribute('themejson')))
+		console.log(theme)
+		addTheme(theme)
+	})
+}
+
+function doThemeResponse(response){
+	responseElement = document.getElementById('themeResponseText')
+	if (response == "ERROR: NOT SUBSCRIBED") {
+		responseElement.innerHTML = 'Error: Animated themes are only available for subscribers. <a style="display:inline-block;" class="btn-primary-md ng-binding" target="_blank" href="https://ropro.io#standard">Upgrade</a>'
+	} else if(response == "SUCCESSFULLY REMOVED") {
+		responseElement.innerHTML = 'Successfully removed profile theme. Other users will not see a theme on your profile!'
+	} else if(response == "SUCCESS") {
+		responseElement.innerHTML = 'Successfully updated profile theme. Other users will be able to see your theme now!'
+	} else {
+		responseElement.innerHTML = 'Error: Unknown error, please try again later.'
+	}
+}
+
+function addThemeBox() {
+	profileContainer = document.getElementsByClassName('profile-container')[0]
+	profileHeader = profileContainer.getElementsByClassName('profile-header')[0]
+	div = document.createElement('div')
+	div.setAttribute('id', 'profileThemesSelectorBox')
+	div.innerHTML += profileThemeBoxHTML
+	profileHeader.parentNode.insertBefore(div, profileHeader.nextElementSibling)
+	document.getElementById('saveTheme').addEventListener("click", async function(){
+		if (this.innerHTML == chrome.i18n.getMessage("SaveTheme")) {
+			responseElement = document.getElementById('themeResponseText').innerHTML = ""
+			backgroundImage = document.getElementById('container-main').style.backgroundImage
+			themeName = backgroundImage.split("themes/")[1].replace('")', "")
+			console.log(themeName)
+			userID = await getStorage("rpUserID")
+			response = await saveTheme(userID, themeName)
+			doThemeResponse(response)
+			this.innerHTML = chrome.i18n.getMessage("RemoveTheme")
+		} else {
+			userID = await getStorage("rpUserID")
+			response = await saveTheme(userID, "remove")
+			doThemeResponse(response)
+			document.getElementById('container-main').style.backgroundImage = ""
+			document.getElementById('container-main').style.backgroundColor = ""
+		}
+	})
+}
+
+function addTheme(theme) {
+	themeName = theme['name']
+	if ('repeat' in theme){
+		repeat = theme['repeat']
+	} else {
+		repeat = "repeat"
+	}
+	if ('width' in theme) {
+		width = theme['width']
+	} else {
+		width = "100%"
+	}
+	if ('color' in theme) {
+		color = theme['color']
+	} else {
+		color = ""
+	}
+	mainContainer = document.getElementById('container-main')
+	profileContainer = document.getElementsByClassName('profile-container')[0]
+	profileContainer.style.padding = "20px"
+	profileContainer.style.paddingTop = "10px"
+	mainContainer.style.backgroundImage = `url(https://ropro.io/themes/${stripTags(themeName)})`
+	mainContainer.style.backgroundSize = width
+	mainContainer.style.backgroundColor = color
+	mainContainer.style.backgroundRepeat = repeat
+	mainContainer.style.borderRadius = "20px"
+	mainContainer.style.padding = "20px"
+	document.getElementById('saveTheme').innerHTML = chrome.i18n.getMessage("SaveTheme")
+	document.getElementById('accoutrements-slider').setAttribute('style', 'width:470px;transform:scale(0.95);margin-left:-7px;')
+}
+
+async function loadThemes() {
+	themesJSON = await fetchThemes()
+	ropro_themes = JSON.parse(themesJSON)
+	addThemeBox()
+	for (i = 0; i < ropro_themes.length; i++){
+		theme = ropro_themes[i]
+		addCard(theme)
+	}
+}
+
+function addOpenThemesButton(){
+	div = document.createElement('div')
+	div.setAttribute("class", "profile-themes-button")
+	div.innerHTML += `<a id="openThemes" style="margin-left:10px;display:inline-block;" class="btn-primary-md ng-binding">Themes<img style="margin:-5px;margin-top:-7px;margin-right:-2px;margin-left:3px;width:20px;" src="https://ropro.io/images/paint_icon_${$('.light-theme').length == 0 ? "dark":"light"}.png"></a>`
+	document.getElementsByClassName('header-title')[0].appendChild(div)
+	document.getElementById('openThemes').addEventListener('click', function(){
+		if (this.innerHTML == `Themes<img style="margin:-5px;margin-top:-7px;margin-right:-2px;margin-left:3px;width:20px;" src="https://ropro.io/images/paint_icon_${$('.light-theme').length == 0 ? "dark":"light"}.png">`) {
+			loadThemes()
+			this.innerHTML = `Close Themes<img style="margin:-5px;margin-top:-7px;margin-right:-2px;margin-left:3px;width:20px;" src="https://ropro.io/images/paint_icon_${$('.light-theme').length == 0 ? "dark":"light"}.png">`
+		} else {
+			this.innerHTML = `Themes<img style="margin:-5px;margin-top:-7px;margin-right:-2px;margin-left:3px;width:20px;" src="https://ropro.io/images/paint_icon_${$('.light-theme').length == 0 ? "dark":"light"}.png">`
+			document.getElementById('profileThemesSelectorBox').remove()
+		}
+	})
+}
+
+function addOpenAnimationsButton(){
+	div = document.getElementsByClassName('profile-themes-button')[0]
+	div2 = document.createElement('div')
+	div2.innerHTML += `<a id="openAnimations" style="margin-left:10px;display:inline-block;" class="btn-primary-md ng-binding">Animations<img style="margin:-5px;margin-top:-7px;margin-right:-2px;margin-left:3px;width:20px;${$('.light-theme').length == 0 ? "" : "filter: invert(1);"}" src="https://ropro.io/images/animation_icon2.png"></a>`
+	div.appendChild(div2.childNodes[0])
+	document.getElementsByClassName('header-title')[0].appendChild(div)
+	document.getElementById('openAnimations').addEventListener('click', function(){
+		if (this.innerHTML == `Animations<img style="margin:-5px;margin-top:-7px;margin-right:-2px;margin-left:3px;width:20px;${$('.light-theme').length == 0 ? "" : "filter: invert(1);"}" src="https://ropro.io/images/animation_icon2.png">`) {
+			//loadAnimations()
+			this.innerHTML = `Close Animations<img style="margin:-5px;margin-top:-7px;margin-right:-2px;margin-left:3px;width:20px;${$('.light-theme').length == 0 ? "" : "filter: invert(1);"}" src="https://ropro.io/images/animation_icon2.png">`
+		} else {
+			this.innerHTML = `Animations<img style="margin:-5px;margin-top:-7px;margin-right:-2px;margin-left:3px;width:20px;${$('.light-theme').length == 0 ? "" : "filter: invert(1);"}" src="https://ropro.io/images/animation_icon2.png">`
+			document.getElementById('profileAnimationsSelectorBox').remove()
+		}
+	})
+}
+
+function addAnimationFrames() {
+	div = document.createElement('div')
+	div.innerHTML = circularProfileAnimationHTML
+	document.getElementsByClassName('profile-avatar-image')[0].appendChild(div.childNodes[0])
+	document.getElementsByClassName('profile-avatar-image')[0].getElementsByClassName('thumbnail-2d-container')[0].style.display = "none"
+	div = document.createElement('div')
+	div.innerHTML = mainProfileAnimationHTML
+	document.getElementsByClassName('thumbnail-holder')[0].appendChild(div.childNodes[0])
+	document.getElementsByClassName('thumbnail-holder')[0].getElementsByClassName('thumbnail-2d-container')[0].style.display = "none"
+	document.getElementsByClassName('avatar-status')[0].style.zIndex = "10000"
+}
+
+function getIdFromURL(url) {
+	return parseInt(url.split("users/")[1].split("/profile")[0])
+}
+
+async function themesMain(){
+	myUserID = await getStorage("rpUserID")
+	pageID = getIdFromURL(location.href)
+	if (myUserID == pageID && await fetchSetting('profileThemes')) {
+		addOpenThemesButton()
+		//addOpenAnimationsButton()
+	}
+	//addAnimationFrames()
+}
+
+themesMain()
+
+window.addEventListener("message", (event) => {
+	console.log(event)
+});
